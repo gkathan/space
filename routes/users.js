16f4d17d0f351aa3cb42ef3512ff3216e76a17ea
Gerold Kathan <gerold.kathan@bwinparty.com>
2015-02-11 16:37:09 +0100

    material design

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+//var phantom = require('phantom');
+
+
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+/*
+	console.log("get called ***");
+	phantom.create(function (ph) {
+	  console.log("phantom.create called ***");
+	  ph.createPage(function (page) {
+	
+		page.open('https://www.bwin.com', function() {
+		page.render('example.png');
+		console.log("***");
+		});
+
+
+
+	  });
+	}, {
+	  dnodeOpts: {
+		weak: false
+	  }
+	});
+*/
+
+
   res.send('respond with a resource');
 });
 
 module.exports = router;
+
+
+
+
+
