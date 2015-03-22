9a39132d5412775f5478d4a5f3f1f18532a2a2fd
cactus <gerold.kathan@gmail.com>
2015-03-22 19:19:12 +0100

    replaced tar with zip

--- bin/scripts/space_deploy.sh
+++ bin/scripts/space_deploy.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 . ~/.bash_profile
 
-PACKAGE='space.tar.gz'
-PACKAGE_TAR='space.tar'
+PACKAGE='space.zip'
 
 # first check whether we have a deploy package..
 if [ -a $PACKAGE ]; then
 	rm -Rf space/app_rollback
 	echo '[s p a c e - deploy] says: parking old version in "space_rollback" folder...'
-	sudo mv  space/app space/app_rollback
+	mv  space/app space/app_rollback
 	echo "[s p a c e - deploy] says: going to unpack and deploy from: $PACKAGE"
 	mkdir space/app -p
-	
 
-	sudo mv $PACKAGE space/app/
+
+	mv $PACKAGE space/app/
 	# transfer the files directory from roll_back (all the uploads,..)
-	
+
 	if [ -d "space/app_rollback/public/files" ]; then
 	  mkdir -p space/app/public/files
-	  cp -a space/app_rollback/public/files space/app/public/files 
+	  cp -a space/app_rollback/public/files space/app/public/files
 	fi
-	
+
 	cd space/app
-	gunzip ./$PACKAGE
-	tar -xvf ./$PACKAGE_TAR
+	unzip ./$PACKAGE
+
 else
 	echo "[s p a c e - deploy] says: SORRY but there is NO $PACKAGE around .... "
 fi
