21b45cbcf53f3273d0a3252a60b6f4d78eead1fe
gerold kathan <gerold.kathan@gmail.com>
2015-04-09 06:59:16 +0200

    playing with jquery-orgchart..

--- routes/organization.js
+++ routes/organization.js
@@ -26,7 +26,7 @@ router.get("/", function(req, res, next) {
 router.get("/facebook", function(req, res, next) {
 	var orgService = require('../services/OrganizationService');
 	orgService.findEmployeesByFunction("Studios",function(employees){
-		
+
 		res.locals.employees = employees;
 		res.render("organization/facebook");
 	});
@@ -37,45 +37,45 @@ router.get("/history/tree/:date", function(req, res, next) {
 	console.log("------------- :date = "+req.params.date);
 	res.locals.orgdate=req.params.date;
 	res.render("organization/org_tree", { title: 's p a c e - organizationchart' });
-	
+
 });
 
 router.get("/history/radial", function(req, res, next) {
 
 	res.send("org radial");
-	
+
 });
 
 router.get("/force", function(req, res, next) {
-
 	res.render("organization/force");
-	
+});
+
+router.get("/simple", function(req, res, next) {
+	res.render("organization/simple");
+	//res.sendfile("org.html");
 });
 
 
+
 router.get("/circlecontain/:collection", function(req, res, next) {
-	
-	
-	
+
+
+
 	res.locals.collection=req.params.collection;
 	res.render("organization/circlecontain",{ title: "s p a c e - "+req.params.collection });
-	
+
 });
 
 router.get("/experiement", function(req, res, next) {
 
 	res.send("experiment");
-	
+
 });
 
 router.get("/experiement2", function(req, res, next) {
 
 	res.send("experiement2");
-	
+
 });
 
 module.exports = router;
-
-
-
-
