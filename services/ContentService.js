0988f4fd49770949402ed6d1dcbfc9b74c840896
cactus <gerold.kathanqgmail.com>
2015-04-14 11:33:16 +0200

    tweaked index news section

--- services/ContentService.js
+++ services/ContentService.js
@@ -21,24 +21,18 @@ var winston=require('winston');
 var logger = winston.loggers.get('space_log');
 
 /**
- * 
+ *
  */
 exports.getLatestSpaceNews = function (context,callback) {
 	logger.debug("getSpaceNews() called: context= "+context);
-	
+
 	var content =  db.collection('content');
-		content.find({'type':'index.spacenews'}).sort({$natural:1}, function (err, docs){
+		content.find({'type':'index.spacenews'}).sort({$natural:-1}, function (err, docs){
 			//sort
-			
+
 			callback(docs[0]);
 			return;
 	});
-	
-	
+
+
 }
-	
-
-
-
-
-
