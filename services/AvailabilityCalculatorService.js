38fe055b5fea932341b87bcf0214a9b360f652b8
cactus <gerold.kathan@gmail.com>
2015-04-30 07:04:22 +0200

    soc incident stuff

--- services/AvailabilityCalculatorService.js
+++ services/AvailabilityCalculatorService.js
@@ -1,5 +1,5 @@
 /**
- * simple content management  service
+ *
  *
  * .....
  */
@@ -10,7 +10,6 @@ var config = require('config');
 var mongojs = require('mongojs');
 
 var _ = require('lodash');
-
 
 var DB=config.database.db;
 var HOST = config.database.host;
@@ -36,6 +35,47 @@ function _getStuff(context,callback) {
 			callback(docs);
 			//return;
 	});
+}
+
+function _calculateOverall(from, to){
+	/*
+	var includeExternal = false;
+	
+	//Set all 0
+	var AvTotalUnplanned = 0.0;
+	var AvTotalPlanned = 0.0;
+
+	//what is YTN?
+	var YTN1total = 0.0;
+	var YTN2total = 0.0;
+
+	//is Forecast used ??
+	// => uses the Target values to calculate some shit
+	// => i think this is NOT used !!
+	var Forecast1total = 0.0;
+	var Forecast2total = 0.0;
+
+	var totalDowntimetotal = 0.0;
+
+	//internal
+	var pldtintotal = 0.0;
+	var unpldtintotal = 0.0;
+	//external
+	var pldtextotal = 0.0;
+	var unpldtextotal = 0.0;
+
+	var technicalDowntimetotal = 0.0;
+	var rounds = 0;
+
+
+	*/
+
+
+	// grab the SOC incidents for the intervall (from to)
+
+	// walk over each incdient
+
+		// do the start stop special handling
 
 
 }
