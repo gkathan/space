4fdae4026e16591e6d30a292c59e77fdf3e911eb
cactus <gerold.kathan@gmail.com>
2015-05-04 19:21:25 +0200

    first prototype of firereport PDF reporting

--- phantomtest.js
+++ phantomtest.js
@@ -1,14 +1,48 @@
 var phantom = require('phantom');
 
-var _url="https://apm.bwinparty.corp/controller/rest/applications/bwin.party/metric-data?metric-path=Business%20Transaction%20Performance%7CBusiness%20Transactions%7Cbetting.api.bwin.be/v2%7CBettingPosBE-BetPlacementService.PlaceBet%7CAverage%20Response%20Time%20%28ms%29&time-range-type=BETWEEN_TIMES&start-time=1427839200000&end-time=1427925600000&output=JSON";
+var _url="http://localhost:3000/dashboard/firereport";
 
 
 phantom.create(function (ph) {
   ph.createPage(function (page) {
+    page.set('settings.userAgent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36');
+    //page.set('viewportSize', {width:1600,height:2400})
+
+    page.set('paperSize', {
+    format: 'A4',
+    orientation: 'portrait',
+    margin: {
+        top: '1cm',
+        left: '1,6cm',
+        right: '1,6cm',
+        bottom: '1cm'
+        }
+      });
+
+
     page.open(_url, function (status) {
-      page.evaluate(function () { return document.title; }, function (result) {
+
+      page.evaluate(function () {
+        $('#username').val('admin');
+        $('#password').val('4dm1n');
+        $('#loginsubmit').click();
+
+
+        return document.title;
+
+        }
+
+        , function (result) {
         console.log('Page title is ' + result);
-        ph.exit();
+
+        setTimeout(function() {
+
+       //page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36';
+         page.render('firereport.pdf', {format: 'pdf', quality: '100',viewportSize:{width:1600,height:1000}},function(){
+           console.log('Page Rendered');
+           ph.exit();
+        });
+      },2000);
       });
     });
   });
