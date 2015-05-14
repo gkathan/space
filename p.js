62e0f8b5a6253175a17c0cf845415b1f18fb6f6e
cactus <gerold.kathan@gmail.com>
2015-05-14 08:01:21 +0200

    cleanup

--- p.js
+++
@@ -1,7 +0,0 @@
-var webPage = require('webpage');
-var page = webPage.create();
-
-page.open('http://space.bwinparty.corp', function(status) {
-  console.log('Status: ' + status);
-  // Do other things here...
-});
